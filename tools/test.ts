06a9a364a79032605326a8d8df81d55735f33854
Greg Deane <greg.deane.dev@gmail.com>
2016-11-18 11:21:55 +0100

    chore(test): add tdd support

--- tools/test.ts
+++ tools/test.ts
@@ -3,11 +3,12 @@ process.env.NODE_ENV = 'test';
 import * as karma from 'karma';
 import paths from '../config/paths';
 
+const tdd = process.argv.indexOf('tdd') !== -1;
 const done = (): any => {
   return process.exit(0);
 };
 
 new karma.Server({
   configFile: paths.config + '/karma.conf.ts',
-  singleRun: true
+  singleRun: !tdd
 }, done).start();
